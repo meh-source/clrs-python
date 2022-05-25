784aee805d5611c729972b876711c62507d9a9d2
meh-source <gmehmail@gmail.com>
2022-05-25 22:25:57 +0100

    change float division to int division

--- ch2/binary_search.py
+++ ch2/binary_search.py
@@ -10,7 +10,7 @@ def binary_search(A, v):
     right = len(A)-1
 
     while left <= right:
-        mid = left + (right-left)/2
+        mid = left + (right-left) // 2
         if A[mid] == v:
             return mid
         if A[mid] < v:
